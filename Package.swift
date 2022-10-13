f715b444628c0c388496197d17357a0940fec77f
Alan DeGuzman <alan.a.deguzman@ehi.com>
2022-10-13 16:35:40 -0500

    spm02

--- Package.swift
+++ Package.swift
@@ -4,43 +4,17 @@ import PackageDescription
 
 let package = Package(
     name: "LicensePlist",
+    platforms: [
+      .iOS(.v13),
+      .watchOS(.v6),
+      .macOS(.v10_15),
+      .tvOS(.v13)
+    ],
     products: [
-        .executable(name: "license-plist", targets: ["LicensePlist"]),
-        .library(name: "LicensePlistCore", targets: ["LicensePlistCore"]),
         .plugin(name: "LicensePlistPlugin", targets: ["LicensePlistPlugin"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git",
-                 from: "1.1.4"),
-        .package(url: "https://github.com/ishkawa/APIKit.git",
-                 from: "5.3.0"),
-        .package(url: "https://github.com/Kitura/HeliumLogger.git",
-                 from: "2.0.0"),
-        .package(url: "https://github.com/behrang/YamlSwift.git",
-                 from: "3.4.4"),
-        .package(url: "https://github.com/Kitura/swift-html-entities.git",
-                 from: "4.0.1"),
-        .package(url: "https://github.com/YusukeHosonuma/SwiftParamTest",
-                 .upToNextMajor(from: "2.0.0")),
-    ],
+    dependencies: [],
     targets: [
-        .target(
-            name: "LicensePlist",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                "LicensePlistCore",
-                "HeliumLogger",
-            ]
-        ),
-        .target(
-            name: "LicensePlistCore",
-            dependencies: [
-                "APIKit",
-                "HeliumLogger",
-                .product(name: "HTMLEntities", package: "swift-html-entities"),
-                .product(name: "Yaml", package: "YamlSwift")
-            ]
-        ),
         .plugin(
           name: "LicensePlistPlugin",
           capability: .buildTool(),
@@ -49,14 +23,6 @@ let package = Package(
           name: "LicensePlistBinary",
           url: "https://github.com/alandeguz/swift-build-tools/releases/download/1.0.4/LicensePlistBinary-macos.artifactbundle.zip",
           checksum: "f2619e91a34b38a525b65d307f7828df6094628ae113c2ab7173eb995c456bb1"
-        ),
-        .testTarget(
-            name: "LicensePlistTests",
-            dependencies: ["LicensePlistCore", "SwiftParamTest"],
-            exclude: [
-                "Resources",
-                "XcodeProjects",
-            ]
-        ),
+        )
     ]
 )
