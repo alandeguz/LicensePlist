f0471650535ebc9e51164a235182d7595650b16c
Alan DeGuzman <aldeguzman@onenorth.com>
2025-07-22 15:05:21 -0500

    for 3.27.1, remove everything except for Swift Package Manager / Xcode plugin support

--- Package.swift
+++ Package.swift
@@ -5,55 +5,12 @@ import PackageDescription
 let package = Package(
     name: "LicensePlist",
     products: [
-        .executable(name: "license-plist", targets: ["LicensePlist"]),
-        .library(name: "LicensePlistCore", targets: ["LicensePlistCore"]),
         .plugin(name: "LicensePlistBuildTool", targets: ["LicensePlistBuildTool"]),
         .plugin(name: "GenerateAcknowledgementsCommand", targets: ["GenerateAcknowledgementsCommand"]),
         .plugin(name: "AddAcknowledgementsCopyScriptCommand", targets: ["AddAcknowledgementsCopyScriptCommand"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git",
-                 from: "1.1.4"),
-        .package(url: "https://github.com/ishkawa/APIKit.git",
-                 from: "5.3.0"),
-        .package(url: "https://github.com/Kitura/HeliumLogger.git",
-                 from: "2.0.0"),
-        .package(url: "https://github.com/Kitura/swift-html-entities.git",
-                 from: "4.0.1"),
-        .package(url: "https://github.com/YusukeHosonuma/SwiftParamTest",
-                 .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/tomlokhorst/XcodeEdit.git",
-                 from: "2.9.0"),
-        .package(url: "https://github.com/jpsim/Yams.git",
-                 from: "5.0.5")
-    ],
+    dependencies: [],
     targets: [
-        .executableTarget(
-            name: "LicensePlist",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                "LicensePlistCore",
-                "HeliumLogger",
-                "XcodeEdit"
-            ]
-        ),
-        .target(
-            name: "LicensePlistCore",
-            dependencies: [
-                "APIKit",
-                "HeliumLogger",
-                .product(name: "HTMLEntities", package: "swift-html-entities"),
-                .product(name: "Yams", package: "Yams")
-            ]
-        ),
-        .testTarget(
-            name: "LicensePlistTests",
-            dependencies: ["LicensePlistCore", "SwiftParamTest"],
-            exclude: [
-                "Resources",
-                "XcodeProjects",
-            ]
-        ),
         .plugin(
             name: "LicensePlistBuildTool",
             capability: .buildTool(),
